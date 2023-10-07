0e281b5da93b1cccaab640ac9393d59132c4ab1e
Roman <roman.zuchowski@gmail.com>
2023-10-07 21:10:19 +0200

    this will add localization and a date service

---
+++ RemindMe/Shared/Controller/HomeViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewModel.swift
+//  RemindMe
+//
+//  Created by Roman Zuchowski on 07.10.23.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    
+    func getObjectsInSection(section: SectionType) -> Int {
+        return 1
+    }
+}
---
+++ RemindMe/Shared/Services/DateService.swift
@@ -0,0 +1,8 @@
+//
+//  DateService.swift
+//  RemindMe
+//
+//  Created by Roman Zuchowski on 07.10.23.
+//
+
+import Foundation
