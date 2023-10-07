063f038382f9a3f98d8b151b8eb8e985681dcdc7
Roman <roman.zuchowski@gmail.com>
2023-10-07 18:44:39 +0200

    initial commit

---
+++ RemindMe (iOS)/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  RemindMe (iOS)
+//
+//  Created by Roman Zuchowski on 07.10.23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
