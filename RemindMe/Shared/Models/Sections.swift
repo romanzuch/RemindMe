b14ef5e6de1e0a8fcf18d339b4ba915e09a51ec7
Roman <roman.zuchowski@gmail.com>
2023-10-07 20:21:30 +0200

    this will add the section type

---
+++ RemindMe/Shared/Models/Sections.swift
@@ -0,0 +1,46 @@
+//
+//  Sections.swift
+//  RemindMe
+//
+//  Created by Roman Zuchowski on 07.10.23.
+//
+
+import Foundation
+
+class Section: Hashable {
+    
+    var type: SectionType
+    var icon: String
+    
+    init(type: SectionType, icon: String) {
+        self.type = type
+        self.icon = icon
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(self)
+    }
+    
+    static func == (lhs: Section, rhs: Section) -> Bool {
+        lhs.type == lhs.type
+    }
+}
+
+enum SectionType: String, Identifiable {
+    case backlog = "SECTION_BACKLOG"
+    case today = "SECTION_TODAY"
+    case soon = "SECTION_SOON"
+    case tags = "SECTION_TAGS"
+    var id: String {
+        return UUID().uuidString
+    }
+}
+
+class Sections {
+    static let sections: [Section] = [
+        Section(type: .backlog, icon: "tray"),
+        Section(type: .today, icon: "calendar.day.timeline.leading"),
+        Section(type: .soon, icon: "calendar"),
+        Section(type: .tags, icon: "tag")
+    ]
+}
---
+++ RemindMe/iOS/Views/SectionView.swift
@@ -0,0 +1,18 @@
+//
+//  SectionView.swift
+//  RemindMe (iOS)
+//
+//  Created by Roman Zuchowski on 07.10.23.
+//
+
+import SwiftUI
+
+struct SectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SectionView()
+}
