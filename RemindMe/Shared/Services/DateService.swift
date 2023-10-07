9838d2db6a009cfb078d85077978409be1ce7605
Roman <roman.zuchowski@gmail.com>
2023-10-07 21:48:33 +0200

    this will add methods to format the date and check whether a date is tomorrow

--- RemindMe/Shared/Services/DateService.swift
+++ RemindMe/Shared/Services/DateService.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+class DateService {
+    static func isDateTomorrow(_ date: Date) -> Bool {
+        let calendar = Calendar.current
+        let tomorrow = calendar.date(byAdding: .day, value: 1, to: Date()) // Get the date for tomorrow
+        return calendar.isDate(date, inSameDayAs: tomorrow!)
+    }
+    static func getDateFormatter() -> DateFormatter {
+        let dateFormatter: DateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.mm.yyyy"
+        return dateFormatter
+    }
+}
