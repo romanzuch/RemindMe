f34704e0d42df4fa30c9f985795a5f2e83059c75
Roman <roman.zuchowski@gmail.com>
2023-10-07 21:47:08 +0200

    this will add methods to sort items per section

--- RemindMe/Shared/Controller/HomeViewModel.swift
+++ RemindMe/Shared/Controller/HomeViewModel.swift
@@ -9,7 +9,51 @@ import Foundation
 
 class HomeViewModel: ObservableObject {
     
-    func getObjectsInSection(section: SectionType) -> Int {
-        return 1
+    func getObjectsInSectionCount(for data: [RemindMeItem], in section: SectionType) -> Int {
+        return getObjectsInSection(for: data, in: section).count
+    }
+    
+    func getObjectsInSection(for data: [RemindMeItem], in section: SectionType) -> [RemindMeItem] {
+        let dateFormatter: DateFormatter = DateService.getDateFormatter()
+        switch section {
+        case .backlog:
+            return data
+        case .today:
+            let todayItems = data.compactMap { item in
+                if let dueDate = item.itemDateDue {
+                    let formattedDate: String = dateFormatter.string(from: dueDate)
+                    if formattedDate == dateFormatter.string(from: Date()) {
+                        return item
+                    } else {
+                        return nil
+                    }
+                } else {
+                    return nil
+                }
+            }
+            return todayItems
+        case .soon:
+            let soonItems = data.compactMap { item in
+                if let dueDate = item.itemDateDue {
+                    if (DateService.isDateTomorrow(dueDate)) {
+                        return item
+                    } else {
+                        return nil
+                    }
+                } else {
+                    return nil
+                }
+            }
+            return soonItems
+        case .tags:
+            let tagsItems = data.compactMap { item in
+                if let tag = item.itemTag {
+                    return item
+                } else {
+                    return nil
+                }
+            }
+            return tagsItems
+        }
     }
 }
