cb25545466518d5c03f230f0e9d53ecdef9a2bfa
Roman <roman.zuchowski@gmail.com>
2023-10-11 19:36:21 +0200

    this will add the home view for macos

--- RemindMe/macOS/Views/HomeView.swift
+++ RemindMe/macOS/Views/HomeView.swift
@@ -6,13 +6,68 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct HomeView: View {
+    
+    @StateObject private var viewModel: HomeViewModel = HomeViewModel()
+    @Environment(\.modelContext) private var modelContext
+    @Query private var items: [RemindMeItem]
+    
+    // MARK: - Add Item Feature
+    @State private var showAddItem: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationSplitView {
+            List {
+                ForEach(Sections.sections, id: \.type) { section in
+                    NavigationLink {
+                        SectionView(
+                            items: viewModel.getObjectsInSection(for: items, in: section.type),
+                            showAddItem: self.$showAddItem
+                        )
+                    } label: {
+                        HStack {
+                            Image(systemName: section.icon)
+                            Text(NSLocalizedString(section.type.rawValue, comment: ""))
+                            Spacer()
+                            Text("\(viewModel.getObjectsInSectionCount(for: items, in: section.type))")
+                        }
+                    }
+                }
+            }
+            .navigationSplitViewColumnWidth(min: 180, ideal: 200)
+            .toolbar {
+                ToolbarItem {
+                    Button(action: {
+                        showAddItem = true
+                    }, label: {
+                        Image(systemName: "plus")
+                    })
+                }
+            }
+        } detail: {
+            Text("Select an item")
+        }
+    }
+    
+    private func addItem() {
+        withAnimation {
+            let newItem = RemindMeItem.getExampleData().randomElement()
+            modelContext.insert(newItem!)
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            for index in offsets {
+                modelContext.delete(items[index])
+            }
+        }
     }
 }
 
 #Preview {
     HomeView()
+        .modelContainer(for: RemindMeItem.self, inMemory: true)
 }
