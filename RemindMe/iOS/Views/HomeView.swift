74f13393ff214bb55af51910642c85bdfb9d6876
Roman <roman.zuchowski@gmail.com>
2023-10-11 19:36:56 +0200

    this will add the home view for ios

--- RemindMe/iOS/Views/HomeView.swift
+++ RemindMe/iOS/Views/HomeView.swift
@@ -6,13 +6,66 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct HomeView: View {
+    
+    @StateObject private var viewModel: HomeViewModel = HomeViewModel()
+    @Environment(\.modelContext) private var modelContext
+    @Query private var items: [RemindMeItem]
+    
+    // MARK: - Add Item Feature
+    @State private var showAddItemView: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationSplitView {
+            List {
+                ForEach(Sections.sections, id: \.type) { section in
+                    NavigationLink {
+                        SectionView(items: viewModel.getObjectsInSection(for: items, in: section.type)
+                        )
+                    } label: {
+                        HStack {
+                            Image(systemName: section.icon)
+                            Text(NSLocalizedString(section.type.rawValue, comment: ""))
+                            Spacer()
+                            Text("\(viewModel.getObjectsInSectionCount(for: items, in: section.type))")
+                        }
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    EditButton()
+                }
+                ToolbarItem {
+                    Button(action: addItem) {
+                        Label("Add Item", systemImage: "plus")
+                    }
+                }
+            }
+        } detail: {
+            Text("Select an item")
+        }
+        .sheet(isPresented: self.$showAddItemView, content: {
+            Text("Hallo")
+        })
+    }
+    
+    private func addItem() {
+        self.showAddItemView.toggle()
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            for index in offsets {
+                modelContext.delete(items[index])
+            }
+        }
     }
 }
 
 #Preview {
     HomeView()
+        .modelContainer(for: RemindMeItem.self, inMemory: true)
 }
